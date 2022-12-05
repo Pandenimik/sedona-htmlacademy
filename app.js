6a1fb9fd817628aea15a0ebcfcd1d84094eef194
Pandenimik <pandenimik@mail.ru>
2022-12-05 08:54:15 +0300

    Закончил главную страницу

---
+++ app.js
@@ -0,0 +1,21 @@
+const btnForm = document.querySelector('.hotel-search_button');
+const form = document.querySelector('.hotel-search-form');
+const toggleForm = function () {
+  form.classList.toggle('hide-form');
+}
+
+btnForm.addEventListener('click', function (e) {
+  e.stopPropagation();
+  toggleForm();
+});
+
+document.addEventListener('click', function (e) {
+  const target = e.target;
+  const its_form = target == form || form.contains(target);
+  const its_btnForm = target == btnForm;
+  const form_is_active = !form.classList.contains('hide-form');
+  
+  if (!its_form && !its_btnForm && form_is_active) {
+    toggleForm();
+  }
+});
