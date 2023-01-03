9f191e80b44ec52adb7572447d5bd6f7168c3f13
Pandenimik <pandenimik@mail.ru>
2023-01-03 17:42:49 +0300

    Подправил JS под БЭМ

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const btnForm = document.querySelector('.hotel-search_button');
-const form = document.querySelector('.hotel-search-form');
+const btnForm = document.querySelector(".hotel-search__button");
+const form = document.querySelector(".hotel-search-form");
 const toggleForm = function () {
-  form.classList.toggle('hide-form');
+  form.classList.toggle("hide-form");
 };
 
-btnForm.addEventListener('click', function (e) {
+btnForm.addEventListener("click", function (e) {
   e.stopPropagation();
   toggleForm();
 });
 
-document.addEventListener('click', function (e) {
+document.addEventListener("click", function (e) {
   const target = e.target;
   const its_form = target == form || form.contains(target);
   const its_btnForm = target == btnForm;
-  const form_is_active = !form.classList.contains('hide-form');
+  const form_is_active = !form.classList.contains("hide-form");
 
   if (!its_form && !its_btnForm && form_is_active) {
     toggleForm();
